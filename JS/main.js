f0a2f98542798f2c50ff0be4c03dc95c2dcd8f09
Andrew Rothbauer <andrew.rothbauer@gmail.com>
2018-08-06 14:57:35 -0400

    Trying a load page

---
+++ JS/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+
+  $('html').append('<h1 class="onLoad">loading...</h1>');
+  
+
+  /*! Fades in page on load */
+  $('body').css('display', 'none');
+  $('body').delay(4000).fadeIn(500);
+
+
+
+
+
+  // Add CSS classes and animations on load with JavaScript
+
+
+
+});
